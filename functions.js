0f0778e77b60fdbaac147040871a06bea993449c
carlos-ssh <carlos.anezeh@me.com>
2020-05-21 16:28:17 -0500

    added new fonts for the project, work with the second navbar and started to work with hide the main navbar on JS

---
+++ functions.js
@@ -0,0 +1,13 @@
+var prevScrollpos = window.pageYOffset;
+document.getElementById("second").style.visibility = "hidden";
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("first").style.top = "0";
+    document.getElementById("second").style.visibility = "hidden";
+  } else {
+    document.getElementById("first").style.top = "-60px";
+    document.getElementById("second").style.visibility = "visible";
+  }
+  prevScrollpos = currentScrollPos;
+}
